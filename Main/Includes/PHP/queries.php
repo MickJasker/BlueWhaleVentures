cf758d40972b1866f0378a6813909e0a8ab26993
Svenpai <svenjanssen12321@gmail.com>
2017-10-09 15:55:42 +0200

    I don't know how to merge
    don't question it

--- Main/Includes/PHP/queries.php
+++ Main/Includes/PHP/queries.php
@@ -3,7 +3,7 @@
 //Each function will prepare an sql-string, and an optional array of variables, 
 //which will be sent to db_functions.
 
-//require "connect.php";
+require "connect.php";
 require "dbFunctions.php";
 
 //TEST-QUERY______________________________________________________________________
@@ -47,6 +47,95 @@ function insertQuery($test2)
 {
 	$sql = "INSERT INTO User(ID) VALUES('$test2')";
 	query($sql);
+}
+
+function getKey($key)
+{
+	$sql = "SELECT Email, Name FROM RegisterKey WHERE `KeyCode` = '$key'";
+	if($data = query($sql))
+	{
+		$db_data = array("true");
+		while($row = $data->fetch_assoc())
+		{
+			$mail = $row["Email"];
+			$name = $row["Name"];
+			array_push($db_data, $mail, $name);
+		}
+		return $db_data;
+	}
+	else
+	{
+		echo "The key doesn't exist";
+		return false;
+	}
+}
+
+function checkKey($key)
+{
+	$sql = "SELECT ID, RoleID FROM RegisterKey WHERE `KeyCode` = '$key'";
+	if($data = query($sql))
+	{
+		while($row = $data->fetch_assoc())
+		{
+			$id = $row["ID"];
+			$role = $row["RoleID"];
+		}
+		
+		$sql = "DELETE FROM `RegisterKey` WHERE ID = '$id'";
+		if($data = query($sql))
+		{
+			return $role;
+		}
+	}
+	else
+	{
+		echo "The key doesn't exist";
+		return false;
+	}
+}
+
+//Creates an account
+function createAccount($role, $user_name, $company_mail, $password)
+{
+	$password = password_hash($password, PASSWORD_DEFAULT);
+	$sql = "INSERT INTO `User`(`RoleID`,`Language`, `Name`, `Locked`) VALUES ('$role','English','$user_name', '1')";
+	
+	if (query($sql))
+	{
+		$sql = "SELECT ID FROM User WHERE Name = '$user_name'";
+		if($data = query($sql))
+		{	
+			while($row = $data->fetch_assoc())
+			{
+				$id = $row["ID"];
+			}
+			
+			if ($id != "")
+			{
+				$sql = "INSERT INTO `Login`(`UserID`, `Email`, `Password`) VALUES ('$id', '$company_mail', '$password')";
+				if (query($sql))
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else 
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return false;
+	}
 }
 
 function selectUser($Email)
@@ -113,6 +202,36 @@ function selectUserName($ID)
 		$row = mysqli_fetch_array($data,MYSQLI_ASSOC);
 		return $row;
 	}
+}
+
+//Admin portal blokken
+function getAdminBlockInfo()
+{
+    $sql = "SELECT ID, Name, Logo FROM Company";
+
+    if($data = Query($sql)) {
+
+        while ($row = $data->fetch_assoc()) {
+            $ID = $row["ID"];
+            $Logo = $row["Logo"];
+            $Name = $row["Name"];
+
+            ?>
+
+            <section id="Block">
+                <a href="../../../Admin_Portal/Pages/clientProfile.php?id=<?php echo $ID ?>">
+                    <div class="BlockLogo">
+                            <img src="../../<?php echo $Logo ?>" alt="Company Logo">
+                    </div>
+                    <div class="BlockTitle">
+                        <h1> <?php echo $Name ?> </h1>
+                    </div>
+                </a>
+            </section>
+
+            <?php
+        }
+    }
 }
 
 ?>
