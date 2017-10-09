e69de50e70c03ce645714d82c4bbe81d6b51a7d8
KevinvHelden <k.v.helden96@hotmail.com>
2017-10-09 16:20:07 +0200

    navbar
    navbar, less en lookingglass img

---
+++ Main/Includes/nav.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="CSS/main.css">
+</head>
+
+<body id="wrapper-nav">
+    <main>
+
+        <div id="nav">
+
+            <img id="logo" src="../Files/Images/logo.svg">
+
+
+            <h4 id="left">
+                Welcome, <span>Startup</span>
+            </h4>
+
+            <input  type="search" id="searchbar" placeholder="Search for Experiment">
+
+            <h4 id="right">
+                Home Profile Logout
+            </h4>
+
+
+
+        </div>
+    </main>
+</body>
+</html>
