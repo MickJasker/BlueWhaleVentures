33429079fc584342a6b523212e3051dbb7eadce3
Mick Jasker <mickjasker@ziggo.nl>
2017-10-09 12:52:18 +0200

    Revert "Index.php making it nice"
    This reverts commit 45eb0a190fc729e87adf2da4f3990b0d15cf613f.

--- index.php
+++ index.php
@@ -1,28 +1,11 @@
 <!doctype html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Test Document</title>
-    <link rel="stylesheet" href="Main/Includes/CSS/main.css">
+<meta charset="utf-8">
+<title>Test Document</title>
 </head>
 
-<body id="wrapper">
-    <main class="row">
-        <div class="col-lg-6 login_col"></div>
-        <div class="col-lg-6 login_col login row">
-
-            <div class="login_block ">
-                <img src="Main/Includes/Images/logo_text.svg" alt="logo">
-                <form action="#" >
-                    <input type="text" placeholder="User name"><br>
-                    <input type="password" placeholder="Password"><br>
-                    <input type="button" class="button" value="Login">
-            </div>
-
-
-        </div>
-
-
-    </main>
+<body>
+<h1>Very Niceeee..</h1>
 </body>
 </html>
