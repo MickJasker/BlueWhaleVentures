b97221abffc853919ff2b7fa8217baaf0b4ce050
MennoBrands <menno_brands@hotmail.com>
2017-10-10 09:36:00 +0200

    Test queties verwijderd

--- Main/Includes/PHP/queries.php
+++ Main/Includes/PHP/queries.php
@@ -6,49 +6,6 @@
 require "connect.php";
 require "dbFunctions.php";
 
-//TEST-QUERY______________________________________________________________________
-// Checks all account information in the database for the right combination.
-// Returns true or false.
-function checkLogin($username, $password)
-{
-	$sql = "SELECT username, password FROM login";
-
-	$data = Query($sql);
-
-	while($row = $data->fetch_assoc()) 
-	{
-		if ($username == $row["username"])
-		{
-			$hash = password_verify($password, $row["password"]);
-			if ($hash != 0)
-			{
-				return true;					
-			}
-		}
-	}
-
-	return false;
-}
-
-//TEST QUERIES________________________________________________________________________
-
-function testQuery($test)
-{
-	$sql = "SELECT * FROM User WHERE ID = '$test'";
-	$data = query($sql);
-
-	while($row = $data->fetch_assoc())
-	{
-		echo "<br>" . $row["ID"];
-	}
-}
-
-function insertQuery($test2)
-{
-	$sql = "INSERT INTO User(ID) VALUES('$test2')";
-	query($sql);
-}
-
 function getKey($key)
 {
 	$sql = "SELECT Email, Name FROM RegisterKey WHERE `KeyCode` = '$key'";
