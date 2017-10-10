ac08accfdfe96626f7b4e2af4ba040ed3541e374
MennoBrands <menno_brands@hotmail.com>
2017-10-10 10:07:16 +0200

    AdminBlokken samengevoegd met index
    AdminBlokken samengevoegd met index.php van de Admin portal. CSS nodig!

--- Admin_Portal/Pages/index.php
+++ Admin_Portal/Pages/index.php
@@ -5,9 +5,47 @@
 <html>
 	<head>
 		<title> Admin Portal </title>
-		<link rel="stylesheet" href="Main/Includes/CSS/main.css">
+		<link rel="stylesheet" href="../../Main/Includes/CSS/main.css">
 	</head>
 	<body id="wrapper-admin">
-		<h1>Hello wurold</h1>
+        <Main>
+            <h1>Salty Mundi</h1>
+            <section id="Block">
+                <a href="#">
+                    <div class="BlockLogo">
+                        <img src="../../Main/Files/Images/blue_plus.png" alt="Add Client">
+                    </div>
+                    <div class="BlockTitle">
+                        <h1> Add Client </h1>
+                    </div>
+                </a>
+            </section>
+
+            <?php
+
+            getCompanyBlockInfo();
+
+            ?>
+
+
+            <section id="Block">
+                <a href="#">
+                    <div class="BlockLogo">
+                        <img src="../../Main/Files/Images/blue_plus.png" alt="Add Client">
+                    </div>
+                    <div class="BlockTitle">
+                        <h1> Add Mentor </h1>
+                    </div>
+                </a>
+            </section>
+
+            <?php
+
+            getMentorBlockInfo();
+
+            ?>
+
+
+        </Main>
 	</body>
 </html>
