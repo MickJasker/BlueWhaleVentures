af017e8c9d1db503340a9ca5d580868ec0f44056
Svenpai <svenjanssen12321@gmail.com>
2017-10-10 09:53:00 +0200

    started working on client profile

--- Admin_Portal/Pages/clientProfile.php
+++ Admin_Portal/Pages/clientProfile.php
@@ -1,1 +1,16 @@
-=
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+        <link rel="stylesheet" href="../../Main/Includes/CSS/main.css">
+    </head>
+
+    <body id="wrapper">
+        <main class="row">
+            <?php
+                require "../../Main/Includes/PHP/queries.php";
+            ?>
+        </main>
+    </body>
+</html>
