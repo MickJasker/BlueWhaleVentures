c9246f1bc042864843e750042b316a7f9de675cc
MennoBrands <menno_brands@hotmail.com>
2017-10-10 11:56:14 +0200

    Sven merge error
    REEE

--- Client_Portal/Pages/index.php
+++ Client_Portal/Pages/index.php
@@ -7,26 +7,26 @@ require '../../Main/Includes/PHP/functions.php';
     <title> Client Portal </title>
     <link rel="stylesheet" href="../../Main/Includes/CSS/main.css">
 </head>
-<body id="wrapper-admin">
-<Main>
-    <h1>Salty Mundi</h1>
-    <section id="Block">
-        <a href="#">
-            <div class="BlockLogo">
-                <img src="../../Main/Files/Images/blue_plus.png" alt="Add Client">
-            </div>
-            <div class="BlockTitle">
-                <h1> Add Experiment </h1>
-            </div>
-        </a>
-    </section>
+    <body id="wrapper-admin">
+        <Main>
+            <h1>Salty Mundi</h1>
+            <section id="Block">
+                <a href="#">
+                    <div class="BlockLogo">
+                        <img src="../../Main/Files/Images/blue_plus.png" alt="Add Client">
+                    </div>
+                    <div class="BlockTitle">
+                        <h1> Add Experiment </h1>
+                    </div>
+                </a>
+            </section>
 
-    <?php
+            <?php
 
-    getExperimentBlockInfo(7)
+            getExperimentBlockInfo(7)
 
-    ?>
+            ?>
 
-</Main>
-</body>
+        </Main>
+    </body>
 </html>
