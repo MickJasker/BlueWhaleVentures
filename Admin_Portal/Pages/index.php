dc1874c0b04ebb538c650fa35d0a0cb93777a638
KevinvHelden <k.v.helden96@hotmail.com>
2017-10-11 09:34:52 +0200

    images bedrijven

--- Admin_Portal/Pages/index.php
+++ Admin_Portal/Pages/index.php
@@ -21,7 +21,7 @@
                 <section id="Block" class="col-lg-4">
                     <a href="#">
                         <div class="BlockLogo">
-                            <img src="../../Main/Files/Images/blue_plus.png" alt="Add Client">
+                            <img src="../../Main/Files/Images/add.svg" alt="Add Client">
                         </div>
                         <div class="BlockTitle">
                             <h1> Add Client </h1>
@@ -43,10 +43,10 @@
                 <section id="Block" class="col-lg-4">
                     <a href="#">
                         <div class="BlockLogo">
-                            <img src="../../Main/Files/Images/blue_plus.png" alt="Add Client">
+                            <img src="../../Main/Files/Images/amazon.png" alt="Add Client">
                         </div>
                         <div class="BlockTitle">
-                            <h1> Add Client </h1>
+                            <h1> Amazon </h1>
                         </div>
                     </a>
                 </section>
@@ -58,10 +58,10 @@
                 <section id="Block" class="col-lg-4">
                     <a href="#">
                         <div class="BlockLogo">
-                            <img src="../../Main/Files/Images/blue_plus.png" alt="Add Client">
+                            <img src="../../Main/Files/Images/poliana.png" alt="Add Client">
                         </div>
                         <div class="BlockTitle">
-                            <h1> Add Client </h1>
+                            <h1> Poliana </h1>
                         </div>
                     </a>
                 </section>
@@ -69,10 +69,10 @@
                 <section id="Block" class="col-lg-4">
                     <a href="#">
                         <div class="BlockLogo">
-                            <img src="../../Main/Files/Images/blue_plus.png" alt="Add Client">
+                            <img src="../../Main/Files/Images/microsoft.png" alt="Add Client">
                         </div>
                         <div class="BlockTitle">
-                            <h1> Add Client </h1>
+                            <h1> Microsoft </h1>
                         </div>
                     </a>
                 </section>
@@ -80,10 +80,36 @@
                 <section id="Block" class="col-lg-4">
                     <a href="#">
                         <div class="BlockLogo">
-                            <img src="../../Main/Files/Images/blue_plus.png" alt="Add Client">
+                            <img src="../../Main/Files/Images/paypal.png" alt="Add Client">
                         </div>
                         <div class="BlockTitle">
-                            <h1> Add Client </h1>
+                            <h1> PayPal </h1>
+                        </div>
+                    </a>
+                </section>
+
+            </div>
+
+            <div class="row">
+
+                <section id="Block" class="col-lg-4">
+                    <a href="#">
+                        <div class="BlockLogo">
+                            <img src="../../Main/Files/Images/poliana.png" alt="Add Client">
+                        </div>
+                        <div class="BlockTitle">
+                            <h1> Poliana </h1>
+                        </div>
+                    </a>
+                </section>
+
+                <section id="Block" class="col-lg-4">
+                    <a href="#">
+                        <div class="BlockLogo">
+                            <img src="../../Main/Files/Images/microsoft.png" alt="Add Client">
+                        </div>
+                        <div class="BlockTitle">
+                            <h1> Microsoft </h1>
                         </div>
                     </a>
                 </section>
