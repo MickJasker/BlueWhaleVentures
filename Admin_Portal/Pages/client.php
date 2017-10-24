e89a99f5844b4746560fc6a6b91d81247405d36e
KevinvHelden <k.v.helden96@hotmail.com>
2017-10-24 13:05:52 +0200

    statische blokken weg

--- Admin_Portal/Pages/client.php
+++ Admin_Portal/Pages/client.php
@@ -1,117 +1,24 @@
-<ul class="list">
-    <div class="content">
+ <?php
+ require '../../Main/Includes/PHP/functions.php';
+ checkSession("Admin");
+ ?>
+ <ul class="list">
+     <div class="content">
+         <li id="Block" class="col-lg-4">
+             <a href="#">
+                 <div class="BlockLogo">
+                     <img src="../../Main/Files/Images/add.svg" alt="Add Client">
+                 </div>
+                 <div class="BlockTitle">
+                     <h1> Add Client </h1>
+                 </div>
+             </a>
+         </li>
 
-        <li id="Block" class="col-lg-4">
-            <a href="#">
-                <div class="BlockLogo">
-                    <img src="../../Main/Files/Images/add.svg" alt="Add Client">
-                </div>
-                <div class="BlockTitle">
-                    <h1> Add Client </h1>
-                </div>
-            </a>
-        </li>
+         <?php
 
-        <li id="Block" class="col-lg-4">
-            <a href="#">
-                <div class="BlockLogo">
-                    <img src="../../Main/Files/Images/google.png" alt="Add Client">
-                </div>
-                <div class="BlockTitle">
-                    <h1> Google </h1>
-                </div>
-            </a>
-        </li>
+         getCompanyBlockInfo();
 
-        <li id="Block" class="col-lg-4">
-            <a href="#">
-                <div class="BlockLogo">
-                    <img src="../../Main/Files/Images/amazon.png" alt="Add Client">
-                </div>
-                <div class="BlockTitle">
-                    <h1> Amazon </h1>
-                </div>
-            </a>
-        </li>
-
-        <li id="Block" class="col-lg-4">
-            <a href="#">
-                <div class="BlockLogo">
-                    <img src="../../Main/Files/Images/poliana.png" alt="Add Client">
-                </div>
-                <div class="BlockTitle">
-                    <h1> Poliana </h1>
-                </div>
-            </a>
-        </li>
-
-        <li id="Block" class="col-lg-4">
-            <a href="#">
-                <div class="BlockLogo">
-                    <img src="../../Main/Files/Images/microsoft.png" alt="Add Client">
-                </div>
-                <div class="BlockTitle">
-                    <h1> Microsoft </h1>
-                </div>
-            </a>
-        </li>
-
-        <li id="Block" class="col-lg-4">
-            <a href="#">
-                <div class="BlockLogo">
-                    <img src="../../Main/Files/Images/paypal.png" alt="Add Client">
-                </div>
-                <div class="BlockTitle">
-                    <h1> PayPal </h1>
-                </div>
-            </a>
-        </li>
-
-        <li id="Block" class="col-lg-4">
-            <a href="#">
-                <div class="BlockLogo">
-                    <img src="../../Main/Files/Images/poliana.png" alt="Add Client">
-                </div>
-                <div class="BlockTitle">
-                    <h1> Poliana </h1>
-                </div>
-            </a>
-        </li>
-
-        <li id="Block" class="col-lg-4">
-            <a href="#">
-                <div class="BlockLogo">
-                    <img src="../../Main/Files/Images/microsoft.png" alt="Add Client">
-                </div>
-                <div class="BlockTitle">
-                    <h1> Microsoft </h1>
-                </div>
-            </a>
-        </li>
-
-    </div>
-</ul>
-
-
-
-
-
-
-
-
-
-
-
-
-<!--<?php
-
-getCompanyBlockInfo();
-
-?>-->
-
-
-<!--<?php
-
-getMentorBlockInfo();
-
-?>-->
+         ?>
+     </div>
+ </ul>
--- Admin_Portal/Pages/mentor.php
+++ Admin_Portal/Pages/mentor.php
@@ -5,6 +5,17 @@ checkSession("Admin");
 <ul class="list">
     <div class="content">
 
+        <li id="Block" class="col-lg-4">
+            <a href="#">
+                <div class="BlockLogo">
+                    <img src="../../Main/Files/Images/add.svg" alt="Add Client">
+                </div>
+                <div class="BlockTitle">
+                    <h1> Add Mentor </h1>
+                </div>
+            </a>
+        </li>
+
         <?php
 
         getMentorBlockInfo();
