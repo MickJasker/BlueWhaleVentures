21802cb3d990a82b3dfc16c9eb7ae2170d635207
Mick Jasker <mickjasker@ziggo.nl>
2017-10-25 10:59:35 +0200

    DesignSheet

--- Client_Portal/Pages/createDesignSheet.php
+++ Client_Portal/Pages/createDesignSheet.php
@@ -8,13 +8,17 @@ checkSession('Company');
     <title> Create a new design sheet </title>
     <link rel="stylesheet" href="../../Main/Includes/CSS/main.css">
 </head>
-	<body id="wrapper-admin">
+	<body id="wrapper-designSheet">
     <header class="wrapper-nav">
         <?php
         require "../../Main/Includes/nav.php"
         ?>
     </header>
 		<Main>
+            <div id="switch">
+                <button id="switchActive">Design sheet</button>
+                <button>Executable</button>
+            </div>
 			<?php
 				getDesignSheetForm("Experiment", $_SESSION['Language']);
 				if (isset($_POST['submitDesignsheet']))
