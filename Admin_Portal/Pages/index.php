7166835515cd59e100b1527083070186a17063ed
Martijn Fleurkens <Martijnfleurkens@gmail.com>
2017-10-30 14:08:11 +0100

    add client and add mentor kind of done

--- Admin_Portal/Pages/index.php
+++ Admin_Portal/Pages/index.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="../../Main/Includes/CSS/main.css">
     <link rel="icon" href="../../Main/Files/Images/icon.svg"/>
     <script src="../../Main/Includes/Javascript/jquery-3.2.1.min.js"></script>
+	<script src="../../Main/Includes/Javascript/functions.js"></script>
 </head>
 <body id="wrapper-admin-body">
 
--- Admin_Portal/Pages/mentor.php
+++ Admin_Portal/Pages/mentor.php
@@ -2,25 +2,10 @@
 require '../../Main/Includes/PHP/functions.php';
 checkSession("Admin");
 ?>
-
-<div id="mentorform" class="mentormodal">
-
-    <!-- Modal content -->
-    <div class="mentormodal-content">
-        <div class="mentormodal-header">
-            <span class="close">&times;</span>
-            <h2>Add Mentor</h2>
-        </div>
-        <div class="mentormodal-body">
-            <form method="POST" action="#">
-                <input id="field" type="text" name="user_name" placeholder="Name"> <br>
-                <input id="field" type="text" name="company_mail" placeholder="E-mail"> <br>
-                <input id="submitbtn" name="generate_mentorkey" type="submit" value="Add mentor">
-            </form>
-			<p>
-			<?php		
+<?php		
 				if (isset($_POST['generate_mentorkey']))
 				{
+					echo '<p>';
 					$user_name = htmlentities(mysqli_real_escape_string($conn, $_POST['user_name']));
 					$company_mail = htmlentities(mysqli_real_escape_string($conn, $_POST['company_mail']));
 					
@@ -36,9 +21,23 @@ checkSession("Admin");
 					{
 						generate_key($company_mail, $user_name, "Mentor");
 					}
+					echo '</p>';
 				}
-			?>
-		</p>
+?>
+<div id="mentorform" class="mentormodal">
+
+    <!-- Modal content -->
+    <div class="mentormodal-content">
+        <div class="mentormodal-header">
+            <span class="close">&times;</span>
+            <h2>Add Mentor</h2>
+        </div>
+        <div class="mentormodal-body">
+            <form method="POST" action="#">
+                <input id="field" type="text" name="user_name" placeholder="Name"> <br>
+                <input id="field" type="text" name="company_mail" placeholder="E-mail"> <br>
+                <input id="submitbtn" name="generate_mentorkey" type="submit" value="Add mentor">
+            </form>
         </div>
     </div>
 
