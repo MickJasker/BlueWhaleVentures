92986bc0270dc22a6d95cb4d2504ba283f0576e2
Svenpai <svenjanssen12321@gmail.com>
2017-10-30 13:09:31 +0100

    merging, dont mind me

--- Client_Portal/Pages/index.php
+++ Client_Portal/Pages/index.php
@@ -11,25 +11,18 @@ CheckSession("Client");
 <body id="wrapper-admin-body">
 <header class="row wrapper-nav">
     <?php
-    require "../../Main/Includes/nav.php"
+    require "../nav_client.php"
     ?>
 </header>
 <Main id="wrapper-admin">
-    <div class="row">
-        <section id="Block">
-            <a href="createExperiment.php">
-                <div class="BlockLogo">
-                    <img src="../../Main/Files/Images/blue_plus.png" alt="Add Client">
-                </div>
-                <div class="BlockTitle">
-                    <h1> Add Experiment </h1>
-                </div>
-            </a>
-        </section>
-
-        <?php getExperimentBlockInfo($_SESSION["CompanyID"]); ?>
-        
-    </div>
+    <ul class="list">
+        <div class="content">
+            <?php getExperimentBlockInfo($_SESSION["CompanyID"]); ?>
+        </div>
+    </ul>
 </Main>
+
+<script src="../../Main/Includes/Javascript/main.js"></script>
+
 </body>
 </html>
