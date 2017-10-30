02d745a21fe11f611c4053cf81a41a963507380d
MennoBrands <menno_brands@hotmail.com>
2017-10-30 15:25:04 +0100

    Some client-mentor shiet

--- Main/Includes/PHP/queries.php
+++ Main/Includes/PHP/queries.php
@@ -792,6 +792,23 @@ function selectCompanyInfo($CompanyID)
             ?>
 
         <div class="wrapper-profile">
+            <div class="mentormodal">
+
+                <!-- Modal content -->
+                <div class="mentormodal-content">
+                    <div class="mentormodal-header">
+                        <span class="close">&times;</span>
+                        <h2>Assign Mentor</h2>
+                    </div>
+                    <div class="mentormodal-body">
+                        <form method="POST" action="#">
+                            <input id="field" type="text" name="user_name" placeholder="Name"> <br>
+                            <input id="field" type="text" name="company_mail" placeholder="E-mail"> <br>
+                            <input id="submitbtn" name="generate_mentorkey" type="submit" value="Add mentor">
+                        </form>
+                    </div>
+                </div>
+            </div>
             <div class="row">
                 <section class="block">
                     <div class="content">
@@ -840,6 +857,15 @@ function selectCompanyInfo($CompanyID)
                     </div>
                     <div class="content">
                         <div class="row">
+
+
+                            <div onclick="assignMentor()" class="mentor-preview col-md-3">
+                                <a class="clientbutton" href="#">
+                                    <img src="../../Main/Files/Images/add.svg" alt="Assign Mentor">
+                                    <h4> Assign Mentor </h4>
+                                </a>
+                            </div>
+
                             <?php selectCompanyMentors($_GET["id"]); ?>
                         </div>
                     </div>
