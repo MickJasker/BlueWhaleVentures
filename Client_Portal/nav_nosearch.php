1a8874bbee99e73a985685fa459308c5d94c4de1
KevinvHelden <k.v.helden96@hotmail.com>
2017-10-30 12:07:46 +0100

    navbar done

--- Client_Portal/nav_nosearch.php
+++ Client_Portal/nav_nosearch.php
@@ -6,23 +6,7 @@
 	<h4 id="left" class="col-md-2">
 		Welcome, <span class="user">Admin</span>
 	</h4>
-
-	<div id="clientswitch" class="col-md-3">
-
-		<h4>
-			<span id="client" class="blue">Client</span>
-		</h4>
-
-		<label class="switch">
-			<input id="checkbox" onclick="selectclientormentor()" type="checkbox">
-			<span class="slider round"></span>
-		</label>
-
-		<h4>
-			<span id="mentor">Mentor</span>
-		</h4>
-
-	</div>
+    
 	<h4 id="right" class="col-md-2">
 		<a href="../../<?php echo $_SESSION['Role'];?>_Portal/Pages/index.php">Home </a>
 		<a href="../../<?php echo $_SESSION['Role'];?>_Portal/Pages/Profile.php">Profile</a>
