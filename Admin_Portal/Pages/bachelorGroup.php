0722f763bc589069e30e265d9157f1ef758778e6
MennoBrands <menno_brands@hotmail.com>
2017-10-31 14:56:24 +0100

    Bacholer add knoppen

--- Admin_Portal/Pages/bachelorGroup.php
+++ Admin_Portal/Pages/bachelorGroup.php
@@ -17,7 +17,23 @@ CheckSession("Admin");
 <Main id="wrapper-admin">
     <ul class="list">
         <div class="content">
-            <?php selectBachelorGroupBlockInfo(secure($_GET['id'])); ?>
+
+            <div onclick="addToBachelorGroup()" id="Block" class="col-lg-4">
+                <a class="clientbutton" href="#">
+                    <div class="BlockLogo">
+                        <img src="../../Main/Files/Images/add.svg" alt="Add To Bachelor Group">
+                    </div>
+                    <div class="BlockTitle">
+                        <h1> Add To Bachelor Group </h1>
+                    </div>
+                </a>
+            </div>
+
+            <?php
+
+               selectBachelorGroupBlockInfo(secure($_GET['id']));
+
+            ?>
         </div>
     </ul>
 </Main>
