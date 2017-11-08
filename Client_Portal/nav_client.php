a43cdf7189d9fe84587fb4be0b6d551b965dc3c9
KevinvHelden <k.v.helden96@hotmail.com>
2017-11-08 13:09:47 +0100

    navbar

--- Client_Portal/nav_client.php
+++ Client_Portal/nav_client.php
@@ -7,7 +7,7 @@
 		Welcome, <span class="user"><?php getUserNames($_SESSION["UserID"]); ?></span>
 	</h4>
 
-    <div id="experimentswitch" class="col-md-3">
+    <div id="experimentswitch" class="col-md-4">
 
         <h4>
             <span id="experiments" class="blue">Experiments</span>
