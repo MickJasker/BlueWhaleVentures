b7adec5dbf96c35a21e78f823d589d6aca0eb779
Bart van Lier <bart@swurl.nl>
2017-11-13 15:25:56 +0100

    Auto stash before merge of "master" and "origin/master"
    Welcome.php stylized GAY

--- Client_Portal/Pages/welcome.php
+++ Client_Portal/Pages/welcome.php
@@ -1,6 +1,6 @@
 <?php
 	require '../../Main/Includes/PHP/functions.php';
-	checkSession('Client');
+	//checkSession('Client');
 ?>
 
 <!DOCTYPE html>
@@ -10,11 +10,12 @@
     <link rel="stylesheet" href="../../Main/Includes/CSS/main.css">
 	<script src="../../Main/Includes/Javascript/functions.js"></script>
 </head>
-	<body id="wrapper-client-body">
-		<h3> Welcome <?php getUserNames($_SESSION["UserID"]); ?>,</h3>	
-		<p> On this portal you are able to add experiments <p>
-		<p> Before you begin please fill some data in about you and your company : <a href="profile.php"> Edit my profile </a> </p>
-		
-		<p> Greetings Maarten </p>
-	</body>
+<body id="wrapper-client-body">
+	<h3> Welcome <?php getUserNames($_SESSION["UserID"]); ?>,</h3>
+	<p> On this portal you are able to add experiments <p>
+	<p> Before you begin please fill some data in about you and your company</br></br><a href="profile.php"> Edit my profile </a> </p>
+	</br>
+	<p> Greetings,</br> Maarten </p>
+</body>
+
 </html>
