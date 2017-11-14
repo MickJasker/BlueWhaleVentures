a4b2d436fe0ca1aa2856e3537dd8d7a6081de482
Svenpai <svenjanssen12321@gmail.com>
2017-11-14 12:52:41 +0100

    replaced userID with companyID in access checks

--- Client_Portal/Pages/bachelorGroup/prototype.php
+++ Client_Portal/Pages/bachelorGroup/prototype.php
@@ -1,7 +1,7 @@
 <?php
 	require '../../../Main/Includes/PHP/functions.php';
 	checkSession('Client');
-    $experimentID = checkExperimentIDBachelor(secure($_GET["experimentID"]), $_SESSION["UserID"]);
+    $experimentID = checkExperimentIDBachelor(secure($_GET["experimentID"]), $_SESSION["CompanyID"]);
 ?>
 
 <!DOCTYPE html>
